5d33029f78df101cea55922c87dccdf8ce748b28
poclitar <poclitar@192.168.1.15>
2016-11-06 22:04:11 +0200

    Lesson 2. Ex 2_2_switch_scanner

---
+++ JavaClassIoanP/src/javaclassioanp/Lesson2ex2_2_switch_scanner.java
@@ -0,0 +1,27 @@
+/*
+ * Lesson 2. Exercise 2_2.
+ */
+package javaclassioanp;
+
+/**
+ * Foloseste scanner and switch
+ * @author poclitar
+ */
+import java.util.Scanner;
+public class Lesson2ex2_2_switch_scanner {
+    public static void main (String[] args) {
+        Scanner InB = new Scanner(System.in); // alocate 'InB' for kbd input 
+        System.out.println ("Guess my name please"); // display info text
+        String NameB = InB.nextLine(); // declare string 'NameB' to host 'InB'
+        switch (NameB){ // evaluate var 'nameB'
+            case "Ioan":
+                System.out.println (NameB + " is my first name :-)");
+                break;
+            case "Poclitaru":
+                System.out.println (NameB + " is my surname :-)");
+                break;
+            default: System.out.println ("Didn't guess any of my names.");
+        } 
+        System.out.println ("See you another time.");
+    } 
+}
