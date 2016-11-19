606d0ec923521957146e3673cd08f95e8182fbb8
poclitar <poclitar@192.168.1.15>
2016-11-19 14:56:42 +0200

    Exercise NumericalCalculator_1. Create a numerical calculator project in Java.

---
+++ JavaClassIoanP/src/numericalcalculator/EraseAll.java
@@ -0,0 +1,20 @@
+/*
+ * Class EraseAll.
+ */
+package numericalcalculator;
+
+/**
+ * Public class to clear the parameters num1 and num2 and result. The 
+ * method's name is EraseAll, and returned variable type is double.
+ * @author poclitar
+ */
+public class EraseAll {
+    public double erasingAll (double num1, double num2){
+        num1 = num2 = 0;
+        double result = 0;
+        System.out.println("num1=0.0; num2=0.0");
+        System.out.println("result is: ");
+        return (result);
+    }
+    
+}
---
+++ JavaClassIoanP/src/numericalcalculator/EraseResult.java
@@ -0,0 +1,17 @@
+/*
+ * Class EraseResult.
+ */
+package numericalcalculator;
+
+/**
+ * Public class to erase the result.
+ * @author poclitar
+ */
+public class EraseResult {
+    public double erasingResult (){
+        double result = 0;
+        System.out.println("result is: ");
+        return (result);
+    }
+    
+}
